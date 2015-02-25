edf844b19b422df4e8c5dcb82e8d63d0e2e03c63
Elias Moosman <elias@youiest.com>
2015-02-25 12:47:48 +0400

    initial commit

---
+++ hello.js
@@ -0,0 +1,32 @@
+if (Meteor.isClient) {
+  // counter starts at 0
+  Session.setDefault('counter', 0);
+
+  Template.hello.helpers({
+    counter: function () {
+      return Session.get('counter');
+    }
+  });
+
+  Template.hello.events({
+    'click button': function () {
+      // increment the counter when button is clicked
+      Session.set('counter', Session.get('counter') + 1);
+    },
+    "click select" : function(evt){
+      // console.log()
+      Session.set("language",evt.currentTarget.value);
+    }
+  });
+}
+
+if (Meteor.isServer) {
+  Meteor.startup(function () {
+     var googleSpreadSheetId = "1PoHE1IGgLMLbvRrHT3m0Ya4vnpWs58lnyr0dWH3-2E4";
+      Meteor.setTimeout(function(){
+        Language.init(googleSpreadSheetId);
+        console.log(Language.get("body.clickMe","hi"));
+      },100);
+
+  });
+}
