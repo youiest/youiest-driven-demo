2ffdbd7768d44e363c3ff624bc7a276503cfb034
Trinisoft Technologies <trinisofttechnologies@gmail.com>
2015-05-27 17:41:28 +0530

    more changes

--- packages/youiest:driven/client.js
+++ packages/youiest:driven/client.js
@@ -1,10 +1,23 @@
 Language.defaultLanguage = navigator.language;
-Session.set("language",navigator.language);
+var startupLang = navigator.language.split("-")[0];
+Session.set("language",startupLang);
 Tracker.autorun(function(){
-  	Meteor.subscribe("language",Session.get("language")); 
+	Meteor.subscribe("language",Session.get("language")); 
 });
 
+Language.localStorage = function(){
+	try{
+		console.log(JSON.parse(localStorage.setItem("language")))
+		return JSON.parse(localStorage.setItem("language"))
+	}
+	catch(err){
+		return false;
+	}
+}
+
 UI.registerHelper("lang", function () {
-	Language.lang = Language.findOne({"_id":Session.get("language")}) || Language.lang;
-	return Language.lang;
+	// Language.lang = Language.findOne({"_id":Session.get("language")});
+	// localStorage.setItem("language", JSON.stringify(Language.lang));
+	return Language.findOne({"_id":Session.get("language")});
 });
+
